3a48b61682fa3ec33324262ee23955c3a20e18ca
Shrey K <srkkadia@gmail.com>
2023-12-11 10:13:37 +0530

    HttpServlet and Basics of ServletRequest interface

---
+++ FirstApp/src/Servlet_Basics/FirstServlet.java
@@ -0,0 +1,17 @@
+package Servlet_Basics;
+
+import java.io.*;
+import javax.servlet.*;
+
+public class FirstServlet extends GenericServlet {
+
+	public void service(ServletRequest req, ServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+		
+		PrintWriter out = res.getWriter();
+		
+		out.print("<html><head><title>Generic Servlet</title></head><body>");
+		out.print("<h1>First Servlet</h1>");
+		out.print("</body></html>");
+	}
+}
---
+++ FirstApp/src/Servlet_Basics/HTTPServlet.java
@@ -0,0 +1,21 @@
+package Servlet_Basics;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HTTPServlet extends HttpServlet {
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		res.setContentType("text/html");
+		
+		PrintWriter out = res.getWriter();
+		
+		out.print("<html><head><title>HTTP Servlet</title><style>body {background-color: orangered; color: yellow;}</style></head><body>");
+		out.print("<h1>HTTP Servlet</h1>");
+		out.print("</body></html>");
+	}
+}
