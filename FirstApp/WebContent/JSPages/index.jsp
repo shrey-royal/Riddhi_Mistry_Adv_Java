297c105032d5b21e1692a89b3bec4ca6e3ccac52
Shrey K <srkkadia@gmail.com>
2024-01-13 10:02:39 +0530

    Filters and Intro to JSP

---
+++ FirstApp/WebContent/JSPages/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>JSP Index Page</title>
+</head>
+<body>
+	<h1>Welcome to JSP</h1>
+	<% out.print(2*5); %>	<%-- Scriptlet Tag --%>
+</body>
+</html>
--- FirstApp/src/servletAdvanced/MyFilter.java
+++ FirstApp/src/servletAdvanced/MyFilter.java
@@ -6,6 +6,7 @@ import java.io.PrintWriter;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -18,16 +19,19 @@ public class MyFilter implements Filter {
 	}
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
-		
+		res.setContentType("text/html");
 		PrintWriter out = res.getWriter();
-		out.print("Filter is invoked before");
-
-		// pass the request along the filter chain
-		chain.doFilter(req, res);
-
-		out.print("Filter is invoked after");
+		
+		
+		String password = req.getParameter("password");
+		if(password.equals("admin")) {
+			chain.doFilter(req, res);
+		} else {
+			out.print("username and password error!");
+			RequestDispatcher rd = req.getRequestDispatcher("FilterForm.html");
+			rd.include(req, res);
+		}
+	
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
